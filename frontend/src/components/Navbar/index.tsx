eb8a9f3ebe2e63ec2911c5a3911243762bccaf8c
Mickael Luiz <mickaelluiz843@gmail.com>
2022-01-14 03:42:10 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './style.css';
+
+function Navbar() {
+    return (
+        <header>
+            <nav className='container'>
+                <div className='dsmovie-nav-content'>
+                    <h1>DSmovie</h1>
+                    <a href="www.google.com">
+                        <div className='dsmovie-contact-container'>
+                            <GithubIcon />
+                            <p className='dsmovie-contact-link'>/devsuperior</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
